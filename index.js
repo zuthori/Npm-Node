461d1f7808ed1775a9c0e4d70c6895a2d5d9903d
Danilo Almeida <danilo00almeida@outlook.com>
2016-11-27 20:38:17 +0000

    node

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require('http'),
 const server = http.createServer((req, res)=>{
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Testando');
+  res.end('Teste');
 });
 
 server.listen(port,()=>{
